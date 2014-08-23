aedc436d19398e11d1b27c1f7e505d0cf14937db
SodaGremlin <gregg@sensorsomething.com>
2014-08-23 16:42:45 -0700

    - Updates for the "Ghost in the light" experiment.

--- testsrc/com.sensorsomething.xbee.jssc/JSSCXbeeTest.java
+++ testsrc/com.sensorsomething.xbee.jssc/JSSCXbeeTest.java
@@ -1,7 +1,7 @@
-package com.rapplogic.xbee.jssc;
+package com.sensorsomething.xbee.jssc;
 
-import com.rapplogic.xbee.JSSCSerialComm;
 import com.rapplogic.xbee.api.XBee;
+import com.sensorsomething.xbee.jssc.JSSCSerialComm;
 import junit.framework.TestCase;
 import org.apache.log4j.BasicConfigurator;
 
@@ -17,17 +17,23 @@ public class JSSCXbeeTest extends TestCase {
 	}
 
 	public void testJSSCStartup() throws Exception {
-		JSSCSerialComm serial = new JSSCSerialComm();
+// create the JSSCSerialComm object, this is the replacement object for the default RxTx one used internally.
+JSSCSerialComm serial = new JSSCSerialComm();
 
-		serial.openSerialPort("COM7", 115200);
+// connect to COM7 at 115200 baud. 8 bits, 1 stop bit, 0 parity is assumed
+serial.openSerialPort("COM7", 115200);
 
-		XBee xbee = new XBee();
+// xbee object that we will connect up too
+XBee xbee = new XBee();
 
-		xbee.initProviderConnection(serial);
+// replaces xbee.open() which uses the RxTx model
+xbee.initProviderConnection(serial);
 
-		assertTrue(xbee.isConnected());
+// test we are connected (This is a jUnit test after all)
+assertTrue(xbee.isConnected());
 
-		xbee.close();
+// close the xBee
+xbee.close();
 	}
 
 	public void testJSSCGetAddress() throws Exception {
