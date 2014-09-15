ce9f3ba9de20fbf5a2bdfa0d256052edc9d209ae
SodaGremlin <gregg@sensorsomething.com>
2014-09-15 13:45:12 -0700

    - removed compile directory - finished host switch implementation for light on/off

--- testsrc/com.sensorsomething.xbee.jssc/JSSCXbeeTest.java
+++ testsrc/com.sensorsomething.xbee.jssc/JSSCXbeeTest.java
@@ -17,23 +17,23 @@ public class JSSCXbeeTest extends TestCase {
 	}
 
 	public void testJSSCStartup() throws Exception {
-// create the JSSCSerialComm object, this is the replacement object for the default RxTx one used internally.
-JSSCSerialComm serial = new JSSCSerialComm();
+		// create the JSSCSerialComm object, this is the replacement object for the default RxTx one used internally.
+		JSSCSerialComm serial = new JSSCSerialComm();
 
-// connect to COM7 at 115200 baud. 8 bits, 1 stop bit, 0 parity is assumed
-serial.openSerialPort("COM7", 115200);
+		// connect to COM7 at 115200 baud. 8 bits, 1 stop bit, 0 parity is assumed
+		serial.openSerialPort("COM7", 115200);
 
-// xbee object that we will connect up too
-XBee xbee = new XBee();
+		// xbee object that we will connect up too
+		XBee xbee = new XBee();
 
-// replaces xbee.open() which uses the RxTx model
-xbee.initProviderConnection(serial);
+		// replaces xbee.open() which uses the RxTx model
+		xbee.initProviderConnection(serial);
 
-// test we are connected (This is a jUnit test after all)
-assertTrue(xbee.isConnected());
+		// test we are connected (This is a jUnit test after all)
+		assertTrue(xbee.isConnected());
 
-// close the xBee
-xbee.close();
+		// close the xBee
+		xbee.close();
 	}
 
 	public void testJSSCGetAddress() throws Exception {
