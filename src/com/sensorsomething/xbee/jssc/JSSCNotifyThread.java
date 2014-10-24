b22b3f3d0abc07cd6be3fd1b5f0f611a71d8152d
SodaGremlin <gregg@sensorsomething.com>
2014-10-23 21:53:23 -0700

    - deployed to BeagleBone black - updated some connection logic to handle linux style strings.

--- src/com/sensorsomething/xbee/jssc/JSSCNotifyThread.java
+++ src/com/sensorsomething/xbee/jssc/JSSCNotifyThread.java
@@ -6,11 +6,7 @@ import jssc.SerialPortException;
 import org.apache.log4j.Logger;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Gregg D. Harrington
- * Date: 7/20/14
- * Time: 4:18 PM
- * To change this template use File | Settings | File Templates.
+ * This class asks the serial port if there is anything on the buffer
  */
 public class JSSCNotifyThread extends Thread {
 	static Logger logger = Logger.getLogger(JSSCNotifyThread.class);
@@ -31,9 +27,14 @@ public class JSSCNotifyThread extends Thread {
 					synchronized (connection) {
 						connection.notify();
 					}
+				} else {
+					Thread.sleep(0, 100); // needed to keep it from spiking out the CPU
 				}
 			} catch (SerialPortException e) {
 				logger.error("Could not check input count, exiting", e);
+				return;
+			} catch (InterruptedException e) {
+				logger.error("Interrupted", e);
 				return;
 			}
 		}
